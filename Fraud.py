e4a730ba0492aafa09b8f0c960435417b7cddf05
dheeraj0000 <114396048+dheeraj0000@users.noreply.github.com>
2024-04-22 14:37:30 +0530

    Update Fraud.py

--- Fraud.py
+++ Fraud.py
@@ -14,7 +14,7 @@ import streamlit as st
 
 # loading the saved model
 
-loaded_model = pickle.load(open('C:/Users/Dheeraj/OneDrive/Desktop/Fraud/Fraud_model.sav', 'rb'))
+loaded_model = pickle.load(open('Fraud_model.sav', 'rb'))
 
 
 # creating a function for Prediction
